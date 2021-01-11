60ccf59ba3597064986457101c51560d2afc4b56
Zmej Serow <zmej.serow@gmail.com>
2021-01-11 11:16:26 +0100

    cname again

--- .eleventy.js
+++ .eleventy.js
@@ -4,5 +4,6 @@ module.exports = function(eleventyConfig) {
     // Find and copy any `jpg` files, maintaining directory structure.
     eleventyConfig.addPassthroughCopy("**/*.jpg");
     eleventyConfig.addPassthroughCopy("css/*");
+    eleventyConfig.addPassthroughCopy("CNAME");
     eleventyConfig.addPlugin(syntaxHighlight);
 };
