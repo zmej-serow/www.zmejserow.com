4532481319d75513cd59d8b713ddeedb6b838c70
Zmej Serow <zmej.serow@gmail.com>
2021-01-20 23:41:53 +0100

    moments mechanics added

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,14 @@
-const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
+const fg = require('fast-glob');
 
 module.exports = function(eleventyConfig) {
-    // Find and copy any `jpg` files, maintaining directory structure.
-    eleventyConfig.addPassthroughCopy("**/*.jpg");
-    eleventyConfig.addPassthroughCopy("css/*");
-    eleventyConfig.addPassthroughCopy("CNAME");
+    eleventyConfig.addCollection('moments_images', _ => fg.sync(['moments/**/*.png', 'moments/**/*.jpg']));
+    eleventyConfig.addPassthroughCopy('**/*.jpg');
+    eleventyConfig.addPassthroughCopy('**/*.png');
+    eleventyConfig.addPassthroughCopy('**/*.mp4');
+    eleventyConfig.addPassthroughCopy('css/*');
+    eleventyConfig.addPassthroughCopy('CNAME');
     eleventyConfig.addPlugin(syntaxHighlight);
+
+    eleventyConfig.setWatchJavaScriptDependencies(false);
 };
