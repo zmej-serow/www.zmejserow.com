291a24f456086a34bc1c22ffa052c78e9ba796ee
Zmej Serow <zmej.serow@gmail.com>
2021-06-21 21:39:42 +0200

    za-gribami-2017

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,17 @@ const fg = require('fast-glob');
 module.exports = function(eleventyConfig) {
     eleventyConfig.addCollection('moments_images', _ => fg.sync(['moments/**/*.png', 'moments/**/*.jpg']));
     eleventyConfig.addCollection('recentposts', collection => collection.getAllSorted().reverse().slice(0, 10))
+    eleventyConfig.addCollection('recentposts2',
+        function(collection) {
+            return collection.getAll().sort(function(a, b) {
+                if (!b.published) { b.published = b.date };
+                if (!a.published) { a.published = a.date };
+                console.log(a);
+                console.log('a →', a.published);
+                console.log('b →', b.published);
+                return b.published - a.published;
+            });
+        });
     eleventyConfig.addPassthroughCopy('**/*.jpg');
     eleventyConfig.addPassthroughCopy('**/*.png');
     eleventyConfig.addPassthroughCopy('**/*.mp4');
